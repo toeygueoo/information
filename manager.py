c7d7b954fcafcfb7fdb3c72b0f6bdd97d406e0ac
toeygueoo <shizhi1970122@126.com>
2021-06-06 13:59:54 +0800

    添加manager管理对象

--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
+from flask_script import Manager
 
 
 app = Flask(__name__)
@@ -18,13 +19,16 @@ class Config(object):
     # 数据库的配置信息
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # 设置加密字符串
     SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
     # flask_session的配置信息
     SESSION_TYPE = "redis"
     SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    # 不设置永久存储
+    SESSION_PERMANENT = False
     SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+    PERMANENT_SESSION_LIFETIME = 86400*2  # session 的有效期，单位是秒
 
 # 添加配置信息
 app.config.from_object(Config)
@@ -36,6 +40,10 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 CSRFProtect(app)
 # 创建session对象
 Session(app)
+# 创建manager管理类
+manager = Manager(app)
+
+
 
 
 @app.route('/index')
@@ -43,4 +51,5 @@ def index():
     return "hello world!"
 
 if __name__ == '__main__':
-    app.run()
+    # python xxx.py runserver -h -p -d
+    manager .run()
